292bddac1f9a54aff8a073753488799921cfc7f0
Edith <eddith@proton.me>
2024-05-20 10:43:04 +0200

    add grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,62 @@
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Grid from './Grid.svelte';
+
+	let pictures = [
+		{
+			images: [
+				{
+					src: 'src/lib/images/V_2023_03_11_Isabel y JoseÌ_alta-723.jpg',
+					alt: 'Kitten 1'
+				},
+				{
+					src: 'src/lib/images/V_2023_07_01_Cristina y Luis-362.jpg',
+					alt: 'Kitten 2'
+				}
+			]
+		},
+		{
+			images: [
+				{
+					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
+					alt: 'Kitten 3'
+				},
+				{
+					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-688.jpg',
+					alt: 'Kitten 4'
+				}
+			]
+		},
+		{
+			images: [
+				{
+					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
+					alt: 'Kitten 3'
+				},
+				{
+					src: 'src/lib/images/V_2023_03_11_Isabel y JoseÌ_alta-723.jpg',
+					alt: 'Kitten 1'
+				},
+				{
+					src: 'src/lib/images/V_2023_07_01_Cristina y Luis-362.jpg',
+					alt: 'Kitten 2'
+				},
+				{
+					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-688.jpg',
+					alt: 'Kitten 4'
+				}
+			]
+		},
+		{
+			images: [
+				{
+					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
+					alt: 'Kitten 3'
+				}
+			]
+		}
+
+	]
 </script>
 
 <svelte:head>
@@ -11,7 +67,7 @@
 </svelte:head>
 
 <section>
-	<Grid/>
+	<Grid pictures={pictures} />
 </section>
 
 <style>
--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,84 +1,74 @@
 <script>
-// @ts-nocheck
+  export let pictures; 
 
-    import Grid  from "svelte-grid";
-    import gridHelp from "svelte-grid/build/helper/index.mjs";
-
-    const { item } = gridHelp;
-    const id = () => "_" + Math.random().toString(36).substr(2, 9);
-
-    let items = [
-      {
-        id: id(),
-        5: item({
-          x: 0,
-          y: 0,
-          w: 2,
-          h: 2,
-        }),
-        3: item({ x: 0, w: 2, h: 2, y: 0 }),
-        1: item({ x: 0, y: 0, w: 1, h: 2 }),
-      },
-      {
-        id: id(),
-        5: item({
-          x: 2,
-          y: 0,
-          w: 3,
-          h: 2,
-        }),
-        3: item({ x: 2, w: 1, h: 2, y: 0 }),
-        1: item({ x: 0, y: 2, w: 1, h: 2 }),
-      },
-  
-      {
-        id: id(),
-        5: item({
-          x: 0,
-          y: 2,
-          w: 5,
-          h: 2,
-        }),
-        3: item({ x: 0, w: 3, h: 2, y: 2 }),
-        1: item({ x: 0, y: 4, w: 1, h: 2 }),
-      },
-    ];
-  
-    const cols = [
-      [1500, 5],
-      [1024, 3],
-      [500, 1],
-    ];
 </script>
 
-<div class="demo-container size">
-    <Grid bind:items={items} rowHeight={100} let:item {cols} let:index>
-      <div class="demo-widget content">
-        <h3>{index}</h3>
-      </div>
-    </Grid>
+<div class="container">
+  {#each pictures as {images}}
+      {#if images.length === 1}
+        <img src={images[0].src} alt={images[0].alt} />
+      {/if}
+      {#if images.length === 2}
+        <div class="duo">
+          {#each images as {src, alt}}
+            <img src={src} alt={alt} />
+          {/each}
+        </div>
+      {/if}
+      {#if images.length === 4}
+        <div class="quatruo">
+          {#each images as {src, alt}, i}
+          {#if i === 0 || i === 3}
+            <img class="small" src={src} alt={alt} />
+          {:else}
+            <img class="large" src={src} alt={alt} />
+            {/if}
+          {/each}
+        </div>
+      {/if}
+  {/each}
+  
 </div>
       
   
 
 <style>
-    .size {
-      max-width: 1100px;
-      width: 100%;
-    }
-    .demo-widget {
-      background: #f1f1f1;
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-    .demo-container {
-      max-width: 800px;;
-      width: 100%;
-    }
+  .duo {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  .quatruo {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  .small {
+    grid-row: span 1;
+    height: 100%;
+  }
+
+  .large {
+    grid-row: span 2;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+   
 </style>
     
     
