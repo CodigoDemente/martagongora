c1ab7151edbb43b8f0e1cacd9763a5aed43b4fc2
Edith <eddith@proton.me>
2024-05-27 21:30:05 +0200

    feat: create Gallery component

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,20 +1,22 @@
 
 <script lang="ts">
+	import Gallery from "../../components/Gallery.svelte";
 	import Grid from "../../components/Grid.svelte";
 	import type { GridBlockData } from "../../types/gridblock";
-
-  	let defaultModal = false;
+	
 
 	let pictures : GridBlockData[] = [
 		{
 			images: [
 				{
 					src: 'src/lib/images/V_2023_03_11_Isabel y JoseÌ_alta-723.jpg',
-					alt: 'Kitten 1'
+					alt: 'Kitten 1',
+					id: "1"
 				},
 				{
 					src: 'src/lib/images/V_2023_07_01_Cristina y Luis-362.jpg',
-					alt: 'Kitten 2'
+					alt: 'Kitten 2',
+					id: "2"
 				}
 			]
 		},
@@ -22,11 +24,13 @@
 			images: [
 				{
 					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
-					alt: 'Kitten 3'
+					alt: 'Kitten 3',
+					id: "3"
 				},
 				{
 					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-688.jpg',
-					alt: 'Kitten 4'
+					alt: 'Kitten 4',
+					id: "4"
 				}
 			]
 		},
@@ -34,19 +38,23 @@
 			images: [
 				{
 					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
-					alt: 'Kitten 3'
+					alt: 'Kitten 3',
+					id: "5"
 				},
 				{
 					src: 'src/lib/images/V_2023_03_11_Isabel y JoseÌ_alta-723.jpg',
-					alt: 'Kitten 1'
+					alt: 'Kitten 1',
+					id: "6"
 				},
 				{
 					src: 'src/lib/images/V_2023_07_01_Cristina y Luis-362.jpg',
-					alt: 'Kitten 2'
+					alt: 'Kitten 2',
+					id: "7"
 				},
 				{
 					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-688.jpg',
-					alt: 'Kitten 4'
+					alt: 'Kitten 4',
+					id: "8"
 				}
 			]
 		},
@@ -54,12 +62,18 @@
 			images: [
 				{
 					src: 'src/lib/images/H_2023_03_11_Isabel y JoseÌ_alta-367.jpg',
-					alt: 'Kitten 3'
+					alt: 'Kitten 3',
+					id: "9"
 				}
 			]
 		}
 
 	]
+	
+	const allImages = pictures.map((picture) => picture.images).flat();
+	let defaultModal = false;
+	let pictureId = "";
+
 </script>
 
 <svelte:head>
@@ -68,22 +82,8 @@
 </svelte:head>
 
 <section>
-	<Grid pictures={pictures} />
-	<h2>Modal Example</h2>
-
-<!-- Trigger/Open The Modal -->
-<button on:click={() => (defaultModal = true)}>Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal" class={defaultModal ? 'modal-open': 'modal'}>
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span on:click={() => (defaultModal = false)} class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
+	<Grid bind:pictureId bind:defaultModal pictures={pictures} />
+	<Gallery gallery={allImages} bind:defaultModal bind:pictureId />
 </section>
 
 <style>
@@ -94,45 +94,4 @@
 		width: 100%;
 	}
 
-.modal {
-	display: none; /* Hidden by default */
-}
-
-.modal-open {
-  display: block;
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
 </style>
