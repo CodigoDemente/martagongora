7c03ac5ce689f04b006a9edc0c5396f17af3cd97
Edith <eddith@proton.me>
2024-05-28 21:07:49 +0200

    feat: styles header for responsive

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import logo from '$lib/images/svelte-logo.svg';
 	import type { MenuEntry } from '../types/menu';
+	import { IconMenu, IconX } from '@tabler/icons-svelte';
+	
 	export let menu : MenuEntry[];
+	$: open = false;
 </script>
 
 <header>
@@ -11,24 +14,33 @@
 		</a>
 	</div>
 
-	<nav>
+	<nav class={open ? 'nav-open' : 'nav'}>
 		<ul>
 			{#each menu as { title, url }}
 			<li>
-				<a href={url}>{title}</a>
+				<a on:click={() => open = false} href={url}>{title}</a>
 			</li>
 			{/each}
 		</ul>
 	</nav>
+
+	<button on:click={() => open = !open}>
+	{#if open}
+        <IconX size={25} stroke={1} />
+    {:else}
+        <IconMenu size={25} stroke={1} />
+    {/if}
+	</button>
 </header>
 
 <style>
 	header {
+		position: relative;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		gap: 2rem;
-		padding-block: 0.8rem;
+		padding-top: 1.5rem;
 	}
 
 	.corner img {
@@ -41,21 +53,29 @@
 		display: none;
 	}
 
-	@media (min-width: 600px) {
-		nav {
-			display: flex;
-		}
+	.nav-open {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 100%;
 	}
 
+
 	ul {
+		position: fixed;
+		top: 0;
 		gap: 2rem;
-		padding: 0;
+		padding: 4rem 0;
 		margin: 0;
 		height: 3em;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		list-style: none;
+		background-color: white;
+		width: 100%;
+		height: 30%;
 	}
 
 	li {
@@ -79,5 +99,38 @@
 
 	a:hover {
 		color: var(--color-theme-1);
+	}
+
+	button {
+		right: 1rem;
+		position: absolute;
+		background: none;
+		border: none;
+		color: var(--color-text);
+		cursor: pointer;
+	}
+
+	@media (min-width: 600px) {
+		nav {
+			display: flex;
+		}
+
+		button {
+			display: none;
+		}
+
+		ul {
+			gap: 2rem;
+			padding: 0;
+			margin: 0;
+			height: 3em;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			list-style: none;
+			position: relative;
+			flex-direction: row;
+			background-color: transparent;
+		}
 	}
 </style>
