af9d9818d8b8a301b72189a0c17763974c27ff20
Alma-Co <eddith@proton.me>
2024-06-30 14:50:48 +0200

    add tabler icon and make carrousel works

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+    import { IconChevronRight, IconChevronLeft, IconX } from '@tabler/icons-svelte';
     export let gallery: {src: string, alt: string, id: string}[];
     export let pictureId: string;
     export let defaultModal: boolean;
     
     $: getPositionInArrayById = gallery.find((image) => image.id === pictureId);
+    // fint the index of the image in the array
+    $: getIndexInArrayById = gallery.findIndex((image) => image.id === pictureId);
+    // find the next image in the array
 </script>
 
 <div class={defaultModal ? 'modal-open': 'modal'}>
 	<div class="modal-content">
+      <button on:click={() => (pictureId = gallery[getIndexInArrayById - 1].id)} class="buttons previous">
+        <IconChevronLeft size={55} stroke={1} />
+      </button>
   		{#if getPositionInArrayById}
-  		<img class="modal-image" src={getPositionInArrayById.src} alt={getPositionInArrayById.alt} />
+  		<img class="modal-image" src={gallery[getIndexInArrayById].src} alt={gallery[getIndexInArrayById].alt} />
   		{/if}
-  		<button on:click={() => (defaultModal = false)} class="close">&times;</button>
+      <button on:click={() => (pictureId = gallery[getIndexInArrayById + 1].id)} class="buttons next">
+        <IconChevronRight size={55} stroke={1} />
+      </button>
+  		<button on:click={() => (defaultModal = false)} class="close"> <IconX size={30} stroke={2} /></button>
 	</div>
 </div>
 
@@ -49,9 +59,9 @@
     }
 
     .close {
-      color: #302f2f;
+      color: #625e5e;
       position: absolute;
-      top: 0;
+      top: 20px;
       right: 20px;
       font-size: 30px;
       border: none;
@@ -61,8 +71,30 @@
 
     .close:hover,
     .close:focus {
-      color: #aaaaaa;
+      color: #c2bebe;
       text-decoration: none;
       cursor: pointer;
     }
+
+    .buttons {
+      color: #625e5e;
+      position: absolute;
+      top: 50%;
+      font-size: 30px;
+      border: none;
+      background: none;
+      cursor: pointer;
+      &.next {
+        right: 20px;
+      }
+      &.previous {
+        left: 20px;
+      }
+    }
+
+    .buttons:hover {
+      color: #c2bebe;
+      cursor: pointer;
+    }
+
 </style>
