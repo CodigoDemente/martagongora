5429cea718ecc04f0235a8ded314b9120f28d8e8
Edith <eddith@proton.me>
2024-07-19 15:14:50 +0200

    feat: header style and footer content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+import Header from '../components/Header.svelte';
 	import type { MenuEntry } from '../types/menu';
 	import './styles.css';
 
@@ -11,6 +12,14 @@
 		{ title: 'About', url: '/about' },
 		{ title: 'Contact', url: '/contact' }
 	];
+
+	let footerContent = {
+		links: [
+			{ title: 'Questions', url: '/questions' },
+			{ title: 'Contact', url: '/contact' }
+		],
+		text: 'Marta Gongora Fotografía ©  Todos los derechos reservados/All rights reserved.'
+	};
 	
 </script>
 
@@ -21,15 +30,7 @@
 		<slot />
 	</main>
 
-	<footer>
-		<div class="links">
-		• 
-		<a href="/questions">Questions</a>
-		• 
-		<a href="/contact">Contact</a>
-		</div>
-		<p>Marta Gongora Fotografía ©  Todos los derechos reservados/All rights reserved.</p>
-	</footer>
+	<Footer {footerContent}/>
 </div>
 
 <style>
@@ -38,65 +39,35 @@
 		flex-direction: column;
 		min-height: 100vh;
 	}
-
-	p {
-		margin: 0;
-		font-size: 10px;
-	}
-
+	
 	main {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 0 3rem;
+		padding: 0 1rem;
 		width: 100%;
 		max-width: 90rem;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 2rem 3rem;
-		background-color: rgb(43, 41, 41);
-		color: white;
-		margin-top: 60px;
-		gap: 2.5rem;
-	}
-
-	a {
-		font-size: 12px;
-		color: white;
-	}
-
-	a:hover {
-		text-decoration: underline;
-	}
-
-	.links {
-		display: flex;
-		width: 100%;
-		max-width: 50rem;
-		justify-content: center;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	@media (min-width: 520px) {
-		footer {
-			margin-top: 80px;
-			font-size: 12px;
+	
+	@media (min-width: 600px) {
+		main {
+			padding: 0 3rem;
 		}
 
-		a {
-		font-size: 14px;
-		}
+	}
 
-		p {
-		font-size: 13px;
+	@media (min-width: 905px) {
+		main {
+			padding: 0 5rem;
+		}
+	}
+
+	@media (min-width: 1240px) {
+		main {
+			padding: 0 6rem;
 		}
 	}
 </style>
