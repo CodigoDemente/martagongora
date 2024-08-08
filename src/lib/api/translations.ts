8276e47e7cc097e8421cd178854be7d5220ea50e
Edith <eddith@proton.me>
2024-08-08 16:52:51 +0200

    create api calls for translations

---
+++ src/lib/api/translations.ts
@@ -0,0 +1,50 @@
+import { browser } from "$app/environment";
+import type { Language } from "../types/language";
+
+export async function fetchTranslationLanguages(): Promise<Language[]> {
+    const url = 'https://marta.cloud.alesanchez.es/language';
+    
+    try {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+
+        const data = await res.json();
+        if (data) {
+            return data;
+        } else {
+            throw new Error('Failed to fetch translation languages');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export async function fetchTranslationFiles(lang: string): Promise<any> {
+    const url = `https://marta.cloud.alesanchez.es/translation/${lang}`;
+
+    try {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+
+        const data = await res.json();
+        if (data) {
+            return data;
+        } else {
+            throw new Error('Failed to fetch translation files');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
