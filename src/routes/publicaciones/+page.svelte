02cf5798ca24d7e204e72e20e321e32c66901330
Edith <eddith@proton.me>
2024-08-15 21:43:11 +0200

    simplify grid component

--- src/routes/publicaciones/+page.svelte
+++ src/routes/publicaciones/+page.svelte
@@ -7,11 +7,7 @@
 	import type { GridBlockData } from '../../types/gridblock';
 	import Loader from '../../lib/components/Loader.svelte';
 
-	let pictures: GridBlockData[] = [
-		{
-			images: []
-		}
-	];
+	let pictures: any[] = [];
 	let isLoading = true;
 	let error = false;
 	let moreImages = true;
@@ -21,37 +17,18 @@
 	let pictureId = '';
 
 	async function getImagesFromInstagram(id?: string) {
-		isLoading = true;
-		try {
+			try {
 			const { data, next } = await fetchInstagramMedia(id);
-
 			moreImages = !!next;
-
-			data.forEach((picture) => {
-				if (pictures.at(-1)!.images.length < 3) {
-					pictures.at(-1)!.images.push({
-						src: picture.media_url,
-						alt: picture.media_type,
-						id: picture.media_url
-					});
-					pictures = pictures;
-				} else {
-					pictures = [
-						...pictures,
-						{
-							images: [
-								{
-									src: picture.media_url,
-									alt: picture.media_type,
-									id: picture.media_url
-								}
-							]
-						}
-					];
-				}
-			});
-
-			nextId = next;
+  			const mappedData = data.map((picture) => {
+                return {
+                    src: picture.media_url,
+                    alt: picture.media_type,
+                    id: picture.media_url
+                };
+            });
+            pictures = [...pictures, ...mappedData];
+            nextId = next;
 		} catch (err) {
 			error = true;
 		} finally {
@@ -59,10 +36,8 @@
 		}
 	}
 
-	$: allImages = pictures.map((picture) => picture.images).flat();
-
 	onMount(async () => {
-		getImagesFromInstagram(undefined);
+		getImagesFromInstagram(nextId);
 	});
 </script>
 
@@ -73,7 +48,7 @@
 
 <section>
 	<Grid bind:pictureId bind:defaultModal {pictures} />
-	<Gallery gallery={allImages} bind:defaultModal bind:pictureId />
+	<Gallery gallery={pictures} bind:defaultModal bind:pictureId />
 
 	{#if !isLoading && !error && moreImages}
 		<div
