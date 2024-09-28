29d2e9f7ad82bc1430bab3f0503c5ee796373934
Edith <eddith@proton.me>
2024-09-28 20:43:44 +0200

    feat: add home in translation store

--- src/lib/services/translationStore.ts
+++ src/lib/services/translationStore.ts
@@ -1,12 +1,13 @@
 import { writable } from 'svelte/store';
 
-export type TranslationKeys = 'about' | 'contact' | 'diary' | 'footer' | 'gallery' | 'terms';
+export type TranslationKeys = 'home' | 'about' | 'contact' | 'diary' | 'footer' | 'gallery' | 'terms';
 
 export type TranslationSection = {
 	[key: string]: string | any;
 };
 
 const translationStore: { [key in TranslationKeys]: TranslationSection } = {
+	home: {},
 	about: {},
 	contact: {},
 	diary: {},
