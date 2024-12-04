e945b736cc886a593551aa3559ab850dbe17a8dc
Ale Sanchez <alesanchezmedina@gmail.com>
2024-12-04 16:06:43 +0100

    chore: 🔊 Add logs with chosen language

--- src/routes/[[lang]]/+layout.server.ts
+++ src/routes/[[lang]]/+layout.server.ts
@@ -14,7 +14,14 @@ export const load: PageServerLoad<OutputDataMenu> = async ({
 	const localeNames = availableLocales.map((locale) => locale.code);
 	const defaultLocale = availableLocales.find((locale) => locale.isDefault)?.code || localeNames[0];
 
+	console.log('Default locale', defaultLocale);
+
+	console.log('Params reeceived', JSON.stringify(params));
+
 	const lang = params.lang || defaultLocale;
+
+	console.log('Lang', lang);
+
 	const translationsMenu = await fetchTranslation(lang, 'menu');
 	const translationsFooter = await fetchTranslation(lang, 'footer');
 
